21d9962b154e167c9372ca927328372623036206
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-23 02:01:44 +0000

    added stripe private and public key to user model

---
+++ database/migrations/2016_03_23_020030_add_stripe_keys_to_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStripeKeysToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            //
+            $table->string('stripe_public_key');
+            $table->string('stripe_private_key');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
+    }
+}
